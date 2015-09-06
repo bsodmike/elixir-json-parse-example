756fdd8b7e4e1221bdd72ee96860a289d395d49d
Michael de Silva <michael@mwdesilva.com>
2015-09-06 20:58:06 +0530

    Print profile info from JSON response

--- lib/json_fetcher.ex
+++ lib/json_fetcher.ex
@@ -1,23 +1,29 @@
 defmodule JsonFetcher do
 
-  def get_url(url) do
+  def fetch(url) do
     HTTPoison.start
-    HTTPoison.get(url)
+    {url, HTTPoison.get(url)}
     |> handle_response
     |> parse_json
+    |> print_details
   end
 
-  defp handle_response(resp) do
+  defp handle_response({url, resp}) do
     case resp do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, body}
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect reason
+      {:ok, %HTTPoison.Response{status_code: status, body: body}} -> {url, :ok, status, body}
+      {:error, %HTTPoison.Error{reason: reason}} -> {url, :error, reason}
     end
   end
 
-  defp parse_json({:ok, body}) do
-    JSON.decode body
+  defp parse_json({_, :error, error}), do: IO.puts "Error: #{error}"
+  defp parse_json({url, :ok, 200, body}), do: {url, JSON.decode(body)}
+  defp parse_json({_, :ok, _, _}), do: raise "Unable to fetch JSON"
+
+  defp print_details({url, info}) do
+    details = ["blog", "email"]
+    user_info = Enum.map(details, fn el -> "#{el}: #{Map.fetch!(elem(info, 1), el)}" end)
+    "https://api.github.com/users/" <> user = url
+    IO.puts "#{user}'s Github Info: " <> Enum.join(user_info, ", ")
   end
 
 end
