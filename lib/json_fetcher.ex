8afe5016890819c94327b2765968a2858d63b3de
Michael de Silva <michael@mwdesilva.com>
2015-09-06 08:54:31 +0530

    Add simple JSON parser

--- lib/json_fetcher.ex
+++ lib/json_fetcher.ex
@@ -1,2 +1,26 @@
 defmodule JsonFetcher do
+
+  def get_url(url) do
+    HTTPoison.start
+    HTTPoison.get(url)
+    |> handle_response
+    |> parse_json
+  end
+
+  defp handle_response(resp) do
+    case resp do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, body}
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
+  end
+
+  defp parse_json(data) do
+    case data do
+      {:ok, body} ->
+        JSON.decode body
+    end
+  end
+
 end
